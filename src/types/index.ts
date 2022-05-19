2e867b4e215ecb1668de4d82c5fd7d3925407fcc
pranshushah <pranshu.shah23@gmail.com>
2022-05-19 13:42:43 +0530

    Interceptors types

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,7 +2,14 @@ export interface AdditionalHttpOptions {
   timeout?: number;
   validateStatus?: (status: number) => boolean;
   json?: unknown;
+  interceptors?: Interceptors;
 }
+
+export type Interceptors = {
+  beforeRequest?: (request: Request) => Request;
+  beforeError?: (response: Response, request: Request) => void;
+  afterResponse?: (response: Response, request: Request) => Response;
+};
 
 export interface HttpOptions extends RequestInit, AdditionalHttpOptions {
   baseUrl?: string | URL;
