87b688d070db9e035d72572bbfc024834231a128
pranshushah <pranshu.shah23@gmail.com>
2022-05-25 19:39:17 +0530

    playground text and clean-up default image.

---
+++ docs/src/components/docs/Playground/index.tsx
@@ -0,0 +1,14 @@
+const playgrounds = [
+  {
+    name: 'CodeSandbox',
+    url: 'https://codesandbox.io/s/http-pro-wcfp64?file=/src/index.ts',
+    img: '',
+  },
+  {
+    name: 'StackBlitz',
+    url: 'https://stackblitz.com/edit/typescript-vgeb2m?file=index.ts',
+    img: '',
+  },
+];
+
+export function Card({}) {}
