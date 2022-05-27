858385f2e781053ca2374c0c7af5255e061a350a
pranshushah <pranshu.shah23@gmail.com>
2022-05-27 12:37:21 +0530

    feat - get Request json docs

---
+++ docs/src/components/docs/get/Json.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+export function Json() {
+  return (
+    <iframe
+      src="https://codesandbox.io/embed/json-mo7815?expanddevtools=1&fontsize=14&moduleview=1&theme=dark&view=editor"
+      style={{
+        width: '100%',
+        height: '500px',
+        border: 0,
+        borderRadius: '4px',
+        overflow: 'hidden',
+      }}
+      title="json"
+      allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+      sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+    ></iframe>
+  );
+}
+
+export function JsonFetch() {
+  return (
+    <iframe
+      src="https://codesandbox.io/embed/gracious-rumple-0rwre7?expanddevtools=1&fontsize=14&hidenavigation=1&theme=dark&view=editor"
+      style={{
+        width: '100%',
+        height: '500px',
+        border: 0,
+        borderRadius: '4px',
+        overflow: 'hidden',
+      }}
+      title="json"
+      allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+      sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+    ></iframe>
+  );
+}
