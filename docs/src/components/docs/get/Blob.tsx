4a83b6a56439a006a6a6f00e7a5ca170cefeefd7
pranshushah <pranshu.shah23@gmail.com>
2022-05-30 16:54:18 +0530

    blob extended

--- docs/src/components/docs/get/Blob.tsx
+++ docs/src/components/docs/get/Blob.tsx
@@ -16,3 +16,21 @@ export function Blob() {
     ></iframe>
   );
 }
+
+export function BlobExtended() {
+  return (
+    <iframe
+      src="https://codesandbox.io/embed/http-pro-blob-extended-ykjbgr?expanddevtools=1&fontsize=14&moduleview=1&theme=dark&view=editor"
+      style={{
+        width: '100%',
+        height: '500px',
+        border: 0,
+        borderRadius: '4px',
+        overflow: 'hidden',
+      }}
+      title="json"
+      allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+      sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+    ></iframe>
+  );
+}
