48841d649647cc2f2cd7ade50aa35ac0f850ad0e
pranshushah <pranshu.shah23@gmail.com>
2022-06-02 12:29:34 +0530

    change the url

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'HTTP-PRO',
   tagline: 'Fetch HTTP requests like pro',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://http-pro.netlify.app/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
