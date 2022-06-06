3ab9c54e3f5a7e94217838a1562322b316e35420
pranshushah <pranshu.shah23@gmail.com>
2022-06-06 17:48:15 +0530

    Json put example.

---
+++ docs/src/components/docs/put/Json.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { CodesandboxExample } from '../../common';
+
+export function Json() {
+  return (
+    <CodesandboxExample baseURL="https://codesandbox.io/embed/http-pro-put-9j3w81" />
+  );
+}
+
+export function JsonFetch() {
+  return (
+    <CodesandboxExample baseURL="https://codesandbox.io/embed/gracious-rumple-0rwre7" />
+  );
+}
