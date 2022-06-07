68bcdae2d0cf1fe9e3c610fa2b8da22392c74f1b
pranshushah <pranshu.shah23@gmail.com>
2022-06-07 19:00:35 +0530

    defaultError example.

--- docs/src/components/docs/ErrorHandling/ErrorHandling.tsx
+++ docs/src/components/docs/ErrorHandling/ErrorHandling.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { CodesandboxExample } from '../../common';
-export function Blob() {
+export function DefaultError() {
   return (
-    <CodesandboxExample baseURL="https://codesandbox.io/embed/http-pro-blob-yo269t" />
+    <CodesandboxExample baseURL="https://codesandbox.io/embed/http-pro-default-error-tnk9ky" />
   );
 }
 
-export function BlobExtended() {
+export function CustomError() {
   return (
     <CodesandboxExample baseURL="https://codesandbox.io/embed/http-pro-blob-extended-ykjbgr" />
   );
