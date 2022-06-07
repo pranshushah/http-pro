b319b3800fb8a235a86c5f67612e4ed121885a95
pranshushah <pranshu.shah23@gmail.com>
2022-06-07 18:21:52 +0530

    text added for error handling.

---
+++ docs/src/components/docs/ErrorHandling/ErrorHandling.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { CodesandboxExample } from '../../common';
+export function Blob() {
+  return (
+    <CodesandboxExample baseURL="https://codesandbox.io/embed/http-pro-blob-yo269t" />
+  );
+}
+
+export function BlobExtended() {
+  return (
+    <CodesandboxExample baseURL="https://codesandbox.io/embed/http-pro-blob-extended-ykjbgr" />
+  );
+}
