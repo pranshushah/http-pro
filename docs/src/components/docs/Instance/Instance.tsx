0c66902f3d44db57a6803ecb32409f8a9a6e77d6
pranshushah <pranshu.shah23@gmail.com>
2022-06-08 12:18:42 +0530

    text in docs

---
+++ docs/src/components/docs/Instance/Instance.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { CodesandboxExample } from '../../common';
+
+export function Instance() {
+  return (
+    <CodesandboxExample baseURL="https://codesandbox.io/embed/http-pro-instance-vm0pei" />
+  );
+}
+
+export function ExtendInstance() {
+  return (
+    <CodesandboxExample baseURL="https://codesandbox.io/embed/http-pro-patch-data-response-h502gk" />
+  );
+}
