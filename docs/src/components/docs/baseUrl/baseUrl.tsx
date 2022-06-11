3b74a1463095b7282ba73c605db049c1d9918b08
pranshushah <pranshu.shah23@gmail.com>
2022-06-11 19:14:26 +0530

    basic text and example baseUrl

---
+++ docs/src/components/docs/baseUrl/baseUrl.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { CodesandboxExample } from '../../common';
+
+export function BaseUrl() {
+  return (
+    <CodesandboxExample baseURL="https://codesandbox.io/embed/baseurl-onobmh" />
+  );
+}
