7a04aa770dd38ddc032bb2e24e1791f5d24aac08
pranshushah <pranshu.shah23@gmail.com>
2022-06-15 12:23:32 +0530

    tests according to new abort-controller

--- src/utils/__test__/executeRequest.test.ts
+++ src/utils/__test__/executeRequest.test.ts
@@ -9,11 +9,14 @@ beforeEach(() => {
 it('should return response without timeout', async () => {
   fetchMock.mockResponseOnce(JSON.stringify({ name: 'pranshu' }));
   const request = new Request('https://www.google.com');
+  const abortController = new globalThis.AbortController();
   const httpOptions: InternalHttpOptions = {
     fetch: globalThis.fetch,
     headers: new Headers(),
   };
-  const res = await (await executeRequest(request, httpOptions)).json();
+  const res = await (
+    await executeRequest(request, httpOptions, abortController)
+  ).json();
   expect(fetchMock).toHaveBeenCalledTimes(1);
   expect(res).toEqual({ name: 'pranshu' });
 });
@@ -22,12 +25,17 @@ it('should return response with timeout parameter', async () => {
   jest.useFakeTimers();
   jest.spyOn(global, 'setTimeout');
   fetchMock.mockResponseOnce(JSON.stringify({ name: 'pranshu' }));
+  const abortController = new globalThis.AbortController();
   const httpOptions: InternalHttpOptions = {
     fetch: globalThis.fetch,
     headers: new Headers(),
+    timeout: 100,
+    signal: abortController.signal,
   };
   const request = new Request('https://www.google.com');
-  const res = await (await executeRequest(request, httpOptions, 100)).json();
+  const res = await (
+    await executeRequest(request, httpOptions, abortController)
+  ).json();
   expect(fetchMock).toHaveBeenCalledTimes(1);
   expect(res).toEqual({ name: 'pranshu' });
   jest.clearAllTimers();
