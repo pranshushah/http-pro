218daf4f41438c831333ece4e75bfa5b5e8cdf9c
pranshushah <pranshu.shah23@gmail.com>
2023-09-29 15:35:22 +0530

    feat: tsup config.

---
+++ tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  sourcemap: true,
+  clean: true,
+  format: ['cjs', 'esm'],
+  dts: true,
+  minify: true,
+  target: 'es2020',
+  outDir: 'dist',
+  platform: 'neutral',
+});
