e2f78ec7e0d7c78b5fa4450447245075c71122f7
pranshushah <pranshu.shah23@gmail.com>
2023-10-12 23:09:08 +0530

    fix: remove try/catch block in addDataInResponse

--- src/utils/addResponseData.ts
+++ src/utils/addResponseData.ts
@@ -6,32 +6,23 @@ export async function addDataInResponse<ResponseType extends any = any>(
 ) {
   //@ts-ignore
   let finalResponse: HpResponse<ResponseType> = response.clone();
-  try {
-    let data: ResponseType;
-    if (options.responseType) {
-      data = (await response[
-        options.responseType
-      ]()) as unknown as ResponseType;
-    } else {
-      data = await response.json();
-    }
-    if (typeof options.validationFunction === 'function') {
-      data = await options.validationFunction(
-        data,
-        options.validationOptions,
-        options.validationSchema
-      );
-    }
-    Object.defineProperty(finalResponse, 'data', {
-      value: data,
-      writable: false,
-    });
-  } catch (e) {
-    // if something goes wrong while parsing data we return empty object.
-    Object.defineProperty(finalResponse, 'data', {
-      value: {},
-      writable: false,
-    });
+  let data: ResponseType;
+  if (options.responseType) {
+    data = (await response[options.responseType]()) as unknown as ResponseType;
+  } else {
+    data = await response.json();
   }
+  if (typeof options.validationFunction === 'function') {
+    data = await options.validationFunction(
+      data,
+      options.validationOptions,
+      options.validationSchema
+    );
+  }
+  Object.defineProperty(finalResponse, 'data', {
+    value: data,
+    writable: false,
+  });
+
   return finalResponse;
 }
