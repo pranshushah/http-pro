9f2f65d3116e550c6f5e6b6385e109ea0899fb68
pranshushah <pranshu.shah23@gmail.com>
2023-10-21 15:03:48 +0530

    fix: edit url in docs.

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -32,7 +32,7 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/pranshushah/http-pro',
+          editUrl: 'https://github.com/pranshushah/http-pro/tree/main/docs',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
